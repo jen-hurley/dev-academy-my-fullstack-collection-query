40932b2201a658a9a20ebbd53f83ef8f9850ef5b
Jen Hurley <j.s.hurley00@gmail.com>
2023-06-19 10:47:46 +1200

    make seed for items

---
+++ server/db/seeds/items.js
@@ -0,0 +1,13 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('wishlist').del()
+  await knex('wishlist').insert([
+    { id: 1, category: 'clothes', item: 'jumper', priority: 1, price: 20 },
+    { id: 1, category: 'tech', item: 'iPad', priority: 2, price: 1000 },
+    { id: 1, category: 'fun', item: 'gig tickets', priority: 3, price: 30 },
+  ])
+}
