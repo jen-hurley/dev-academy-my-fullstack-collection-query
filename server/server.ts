4acfd66c4ab11c4d1c394dc311c2d17d00bc6ad4
Jen Hurley <j.s.hurley00@gmail.com>
2023-06-19 11:37:36 +1200

    set up server side api path

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,22 @@
+import { join } from 'node:path'
+
 import express from 'express'
+
 import path from 'path'
+
+import wishlistRoutes from './routes/wishlist'
 
 const server = express()
 
 server.use(express.json())
+
+server.use(express.static(join(__dirname, 'public')))
+server.use(express.json())
+
+server.use('/api/v1/wishlist', wishlistRoutes)
+server.use('/api/*', (req, res) => {
+  res.sendStatus(404)
+})
 
 if (process.env.NODE_ENV === 'production') {
   server.use('/assets', express.static(path.resolve(__dirname, '../assets')))
