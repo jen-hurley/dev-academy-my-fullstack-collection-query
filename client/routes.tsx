d4b64dc093cc44fa2e6a92996c503d7b1e851d84
Jen Hurley <j.s.hurley00@gmail.com>
2023-06-19 12:06:41 +1200

    make components for wishlist and home page

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,6 +1,8 @@
 import { Route, createRoutesFromElements } from 'react-router-dom'
 
 import App from './components/App'
+import Home from './components/Home'
+import Wishlist from './components/Wishlist'
 
 export const routes = createRoutesFromElements(
   <Route element={<App />}>
